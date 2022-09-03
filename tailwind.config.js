6148ece78a5e8f672dfc155e6e7ea86c42c469b6
AnushkaaSharma <sanushkaa4@gmail.com>
2022-09-03 18:47:07 +0545

    added styleguides

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./dist/**/*.html"], 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          '100': '#FAFAFA',
+          '200': '#F7F7F7',
+          '300': '#F2F2F2',
+          '400': '#D8D8D8',
+          '500': '#C2C2C2',
+          '600': '#A1A1A1',
+          '700': '#7B7B7B',
+          '800': '#484848',
+          '900': '#2C2C2C',
+        }
+      },
+      fontSize: {
+        'sm': ['14px', '20px'],
+        'p': ['16px', '24px'],
+        'h6': ['20px', '32px'],
+        'h4': ['32px', '44px'],
+      },
+      fontWeight: {
+        light: 300,
+        regular: 400,
+        semibold: 600,
+        bold: 700,
+      },
+      fontFamily: {
+        body: ['Mulish']
+      }
+    },
   },
   plugins: [],
 }
